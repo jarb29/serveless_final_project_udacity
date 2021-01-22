fdd8ec896fc84e00725dea6b53961b012fabeb92
jarb29 <jarb29@gmail.com>
2021-01-22 05:55:08 -0300

    type: lambda body: adding get todos

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -4,13 +4,16 @@ import * as AWS  from 'aws-sdk'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 
-const groupsTable = process.env.GROUPS_TABLE
+const TodosTable = process.env.TODOS_TABLE
+  
+
+// TODO: Get all TODO items for a current user
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Processing event: ', event)
 
   const result = await docClient.scan({
-    TableName: groupsTable
+    TableName: TodosTable
   }).promise()
 
   const items = result.Items
