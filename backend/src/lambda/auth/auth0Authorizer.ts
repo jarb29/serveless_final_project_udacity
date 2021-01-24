a2180b0224513ce1bc016491a39cee1a93cbf341
jarb29 <jarb29@gmail.com>
2021-01-24 10:04:41 -0300

    type: signeurl body: adding lambda

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -3,8 +3,8 @@ import 'source-map-support/register'
 
 import { verify, decode } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-import Axios from 'axios'
-import { Jwt } from '../../auth/Jwt'
+// import Axios from 'axios'
+// import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
@@ -72,7 +72,7 @@ export const handler = async (
   }
 }
 
-function verifyToken(authHeader: string): JwtPayload {
+async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   // const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
