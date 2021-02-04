b13ffc4fbc0301764b9d768570050d582e623579
jarb29 <jarb29@gmail.com>
2021-02-04 17:58:54 -0300

    type: fixing validator body: ading validator to createtodo

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '9clw6r6r24'
+const apiId = 'lwio44i3sa'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
